d41b90b8a5d968ef76315160d30261ac23f5b972
Demianovq <110334010+Demianovq@users.noreply.github.com>
2023-03-24 19:40:17 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,81 @@
+import { fetchMov } from './services/fetchmovie';
+import { useState, useEffect } from 'react';
+import { Button } from './button/button';
+import { List } from './movieslist/list';
+import { moviesMapper } from '../helpers/moviesmapper';
+import { Loader } from './loader/loader';
+import { Modal } from './Modal/Modal';
+
+export const App = () => {
+  const [isMoviesShown, setIsMoviesShown] = useState(false);
+  const [page, setPage] = useState(1);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(``);
+  const [posterImage, setPosterImage] = useState(``);
+
+  useEffect(() => {
+    if (!isMoviesShown) {
+      setMovies([]);
+
+      // Для пуша на гит \/
+      setPage(1);
+      return;
+    }
+    setIsLoading(true);
+    fetchMov(page)
+      .then(({ data: { results } }) => {
+        setMovies(prev => [...prev, ...moviesMapper(results)]);
+        setIsError(`null`);
+      })
+      .catch(error => {
+        console.log(isError);
+        setIsError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  }, [isMoviesShown, page, isError]);
+
+  const ShowMoviesList = () => {
+    setIsMoviesShown(prev => !prev);
+  };
+
+  const onDelete = movieId => {
+    setMovies(prev => prev.filter(({ id }) => id !== movieId));
+  };
+  const changeStatus = movieId => {
+    setMovies(prev =>
+      prev.map(movie => {
+        if (movie.id === movieId) {
+          return { ...movie, isWatched: !movie.isWatched };
+        }
+        return movie;
+      })
+    );
+  };
+  const openAModal = poster => {
+    setPosterImage(poster);
+  };
+
+  const closeAModal = () => {
+    setPosterImage('');
+  };
+
+  return (
+    <div>
+      <Button
+        text={isMoviesShown ? 'Hide' : 'Show'}
+        clickHandler={ShowMoviesList}
+      />
+      {isLoading && <Loader />}
+      {isMoviesShown && (
+        <List
+          movies={movies}
+          onDelete={onDelete}
+          changeStatus={changeStatus}
+          openAModal={openAModal}
+        />
+      )}
+      {posterImage && <Modal image={posterImage} closeAModal={closeAModal} />}
+    </div>
+  );
+};
